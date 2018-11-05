356e4fed542286475c2269c385e05ef73cf74694
juanbarata <juan.barata.si@gmail.com>
2018-11-05 01:45:53 -0200

    Utilização do getElementById()

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,7 +1,18 @@
 // Fórmula IMC => peso / altura * altura
 
-var peso = 100;
-var altura = 2.0;
-var imc = peso / (altura*altura);
+var tdPeso = document.getElementById('peso-1');
+var tdAltura = document.getElementById('altura-1');
+var tdImc = document.getElementById('imc-1');
 
-console.log(imc);
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+if(altura != 0){
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+    console.log(imc);
+}else{
+    console.log("A altura deve ser diferente de zero!");
+}
+
+console.log(peso);
